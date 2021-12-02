492fc8f89c09534495beb3138ad61ab95d838648
Brandon Quan <30513301+bquan057@users.noreply.github.com>
2021-12-02 02:01:02 -0700

    Updated App class.

--- src/App.java
+++ src/App.java
@@ -1,6 +1,10 @@
 public class App {
 
     public static void main(String[] args){
-
+    	MovieNewsView movieNews = new MovieNewsView();
+    	MovieNewsController movieNewsController = new MovieNewsController(movieNews);
+    	
+    	MainMenu window = new MainMenu(movieNews);
+		window.frame.setVisible(true);
     }
 }
